1e966c3d25e8911d497acfc0578729e3dbf37b90
Dima Hudovec <hudovecd@gmail.com>
2017-03-25 19:54:04 +0300

    Added score and tests for it

---
+++ Assets/script/Editor/Tests.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class Tests {
+
+	[Test]
+	public void ScoreTest()
+	{
+		var scoreHandler = new VirtualButtonEventHandler ();
+		scoreHandler.CalculateScore (true);
+
+		Assert.AreEqual (1, scoreHandler.Score);
+	}
+
+	[Test]
+	public void ScoreTestMinus()
+	{
+		var scoreHandler = new VirtualButtonEventHandler ();
+		scoreHandler.CalculateScore (false);
+
+		Assert.AreEqual (0, scoreHandler.Score);
+	}
+}
---
+++ Assets/script/Initializer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Initializer : MonoBehaviour 
+{
+	public Camera LeftCamera;
+	public Camera RightCamera;
+	public GameObject canvas;
+	public VirtualButtonEventHandler handler;
+	public bool paused;
+
+	private Text leftScore;
+	private Text rightScore;
+
+	private void Start()
+	{
+		paused = false;
+		var leftCanvas = GameObject.Instantiate (canvas).GetComponent<Canvas> ();
+		leftCanvas.transform.parent = transform;
+		leftCanvas.renderMode = RenderMode.ScreenSpaceCamera;
+		leftCanvas.worldCamera = LeftCamera;
+		leftScore = leftCanvas.GetComponentInChildren<Text> ();
+		var rightCanvas = GameObject.Instantiate (canvas).GetComponent<Canvas> ();
+		rightCanvas.transform.parent = transform;
+		rightCanvas.renderMode = RenderMode.ScreenSpaceCamera;
+		rightCanvas.worldCamera = RightCamera;
+		rightScore = rightCanvas.GetComponentInChildren<Text> ();
+	}
+
+	private void Update()
+	{
+		leftScore.text = "Score: " + handler.Score;
+		rightScore.text = leftScore.text;
+		if(Input.GetKeyUp(KeyCode.Escape))
+		{  
+			if(!paused)
+			{  
+				Time.timeScale = 0;  
+				paused=true;  
+			}
+			else
+			{  
+				Time.timeScale = 1;  
+				paused=false;  
+			} 
+		} 
+	}
+}
