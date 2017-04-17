adbcaa2865f505dcaf8e71f10f126a1ccf43a581
Valeria450501 <popenya.valera@gmail.com>
2017-04-17 17:09:32 +0300

    Added a delightful menu

---
+++ Assets/script/MainMenuButtonHandler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuButtonHandler : MonoBehaviour {
+
+    public Button playButton;
+    public Button optionsButton;
+    public Button exitButton;
+
+    void Start()
+    {
+        Button buttonPlay = playButton.GetComponent<Button>();
+        Button buttonOption = optionsButton.GetComponent<Button>();
+        Button buttonExit = exitButton.GetComponent<Button>();
+        buttonPlay.onClick.AddListener(TaskOnClickPlay);
+        buttonOption.onClick.AddListener(TaskOnClickOption);
+        buttonExit.onClick.AddListener(TaskOnClickExit);
+    }
+
+    void TaskOnClickPlay()
+    {
+        Debug.Log("You have clicked the play button!");
+        ScoreClass.Score = 0;
+        Application.LoadLevel("AR4good");
+    }
+
+    void TaskOnClickExit()
+    {
+        Debug.Log("You have clicked the exit button!");
+        Application.Quit();
+    }
+
+    void TaskOnClickOption()
+    {
+        Debug.Log("You have clicked the option button!");
+    }
+}
---
+++ Assets/script/PauseMenuButtonHandler.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenuButtonHandler : MonoBehaviour {
+
+    public Button continueButton;
+    public Button saveButton;
+    public Button optionButton;
+    public Button menuButton;
+
+    void Start()
+    {
+        Button buttonContinue = continueButton.GetComponent<Button>();
+        Button buttonSave = saveButton.GetComponent<Button>();
+        Button buttonOption = optionButton.GetComponent<Button>();
+        Button buttonMenu = menuButton.GetComponent<Button>();
+        buttonContinue.onClick.AddListener(TaskOnClickContinue);
+        buttonSave.onClick.AddListener(TaskOnClickSave);
+        buttonOption.onClick.AddListener(TaskOnClickOption);
+        buttonMenu.onClick.AddListener(TaskOnClickMenu);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.LoadLevel("AR4good");
+    }
+
+    void TaskOnClickContinue()
+    {
+        Debug.Log("You have clicked the pcontinue button!");
+        Application.LoadLevel("AR4good");
+    }
+
+    void TaskOnClickSave()
+    {
+        Debug.Log("You have clicked the save button!");
+    }
+
+    void TaskOnClickOption()
+    {
+        Debug.Log("You have clicked the option button!");
+    }
+
+    void TaskOnClickMenu()
+    {
+        Debug.Log("You have clicked the menu button!");
+        Application.LoadLevel("Menu");
+    }
+}
